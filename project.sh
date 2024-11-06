2859d1573d0d68fee5d511b3681abc94e0d87fa1
aljazmc <mlinaric.aljaz@gmail.com>
2024-11-06 08:07:14 +0200

    style: change indentation

--- project.sh
+++ project.sh
@@ -19,20 +19,20 @@ PROJECT_GID=$(id -g)
 if [ ! -f docker-compose.yml ]; then
     cat << EOF > docker-compose.yml
 services:
-    node:
-        image: node:current-alpine
-        working_dir: /home/node
-        volumes:
-            - .:/home/node
-        environment:
-            NODE_ENV:   development
-            PATH:     "/home/node/.yarn/bin:/home/node/node_modules/.bin:\$PATH"
-        network_mode: host
+  node:
+    image: node:current-alpine
+    working_dir: /home/node
+    volumes:
+      - .:/home/node
+    environment:
+      NODE_ENV: development
+      PATH: "/home/node/.yarn/bin:/home/node/node_modules/.bin:\$PATH"
+    network_mode: host
 EOF
 
   if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     echo "Adding user configuration line to docker-compose.yml for GNU/Linux users."
-    sed -i "3 a \ \ \ \ \ \ \ \ user\:\ $PROJECT_UID\:$PROJECT_GID" docker-compose.yml
+    sed -i "3 a \ \ \ \ user\:\ $PROJECT_UID\:$PROJECT_GID" docker-compose.yml
   fi
 
 fi
