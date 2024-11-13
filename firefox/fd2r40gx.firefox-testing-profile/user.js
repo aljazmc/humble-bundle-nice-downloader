0235b6768ba0c9fae32e6ce89e312e8e5d2a40de
aljazmc <mlinaric.aljaz@gmail.com>
2024-11-13 11:53:15 +0200

    chore: add custom settings for firefox testing profile

---
+++ firefox/fd2r40gx.firefox-testing-profile/user.js
@@ -0,0 +1,4 @@
+// Custom configuration for testing humble-bundle-nice-downloader
+user_pref("extensions.signatureCheckpoint", 0);
+user_pref("xpinstall.signatures.required", false);
+user_pref("xpinstall.whitelist.required", false);
