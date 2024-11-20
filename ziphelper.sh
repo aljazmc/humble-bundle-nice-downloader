d9532abd153f24be6b0009f278bc1c5bd4c768a2
aljazmc <mlinaric.aljaz@gmail.com>
2024-11-20 11:46:01 +0200

    docs: improve clarity

--- ziphelper.sh
+++ ziphelper.sh
@@ -8,27 +8,35 @@
 ## Check if OS is GNU/Linux or exit
 
 if [[ "$OSTYPE" != "linux-gnu"* ]]; then
-    echo "Script runs only on GNU/Linux OS. Exiting..."
-    exit
+  echo "Script runs only on GNU/Linux OS. Exiting..."
+  exit
 fi
 
-## Check if directory humble-bundle-nice-downloader exists or exit
-
 if [[ ! -d humble-bundle-nice-downloader ]]; then
-    echo "Directory humble-bundle-nice-downloader doesn't exist. Exiting..."
-    echo ""
-    echo "You should start \"yarn build-watch\" before running this script"
-    exit
+
+  ## Check if directory humble-bundle-nice-downloader exists or exit
+
+  echo "Directory humble-bundle-nice-downloader doesn't exist. Exiting..."
+  echo ""
+  echo "You should start \"yarn build-watch\" before running this script"
+  exit
 else
-   cd humble-bundle-nice-downloader && \
-   7z u ../firefox/fd2r40gx.firefox-testing-profile/extensions/\{e1ea7933-f4a5-4d96-8838-90950e98d093\}.xpi ./* -r
-   cd ..
+   
+  ## Build initial xpi archive in Firefox testing profile extensions folder
+
+  cd humble-bundle-nice-downloader && \
+  7z u ../firefox/fd2r40gx.firefox-testing-profile/extensions/\{e1ea7933-f4a5-4d96-8838-90950e98d093\}.xpi ./* -r
+  cd ..
 fi
 
 while true;
- do 
-   watch -n 3 -d -t -g ls -lR humble-bundle-nice-downloader &&
-   cd humble-bundle-nice-downloader && \
-   7z u ../firefox/fd2r40gx.firefox-testing-profile/extensions/\{e1ea7933-f4a5-4d96-8838-90950e98d093\}.xpi ./* -r && \
-   cd ..
- done
+do
+
+  ## Watch humble-bundle-nice-downloader folder and 
+  ## rebuild xpi archive after each change
+
+  watch -n 3 -d -t -g ls -lR humble-bundle-nice-downloader &&
+  cd humble-bundle-nice-downloader && \
+  7z u ../firefox/fd2r40gx.firefox-testing-profile/extensions/\{e1ea7933-f4a5-4d96-8838-90950e98d093\}.xpi ./* -r && \
+  cd ..
+done
