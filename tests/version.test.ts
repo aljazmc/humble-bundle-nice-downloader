15e0d5304a54ef6ec3d411eb03c0a94ec75cc1e5
aljazmc <mlinaric.aljaz@gmail.com>
2024-11-25 09:41:09 +0200

    test: change whitespaces

--- tests/version.test.ts
+++ tests/version.test.ts
@@ -2,5 +2,7 @@ import { version as packageVersion } from "../package.json";
 import { version as manifestVersion } from "../src/manifest.json";
 
 test('packageVersion matches manifestVersion', () => {
+
   expect(packageVersion.match(manifestVersion));
+
 });
